3643e964b22d97149e1143071242cd0cc1f879b1
Hauson Chen <hauson621@hotmail.com.tw>
2024-09-18 15:49:16 +0800

    1st version

---
+++ accounting.py
@@ -0,0 +1,15 @@
+products = []
+while True:
+	name = input('please input merchandise name: ')
+	if name == 'q':
+		break
+	price = input('please input merchandise price: ')
+	# p = []		# small list
+	# p.append(name)
+	# p.append(price)
+	p = [name, price]	# replace rows 7~9
+	# products.append(p)	# big list, p is included in products
+	products.append([name, price]) # omit p list
+print(products)
+print(products[0][0])
+print(products[1][1])
