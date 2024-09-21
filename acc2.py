edef4be4b02a5a402a4e1181cf43c3cfd7881adc
Hauson Chen <hauson621@hotmail.com.tw>
2024-09-21 09:29:05 +0800

    function structure

--- acc2.py
+++ acc2.py
@@ -1,36 +1,48 @@
 import os # loading operating system
 
-products = []
-
-if os.path.isfile('products.csv'): # check if csv file exists
-	print('file found')
-	# read from csv
-	with open('products.csv', 'r', encoding = 'utf-8') as f:
-		for line in f:
-			if 'merchandise,price' in line:
-				continue
-			name, price = line.strip().split(',')
-			products.append([name, price])
-	print(products)
-else:
-	print('file not found')
+# read file
+def read_file(filename):
+	products = []
+	if os.path.isfile(filename): # check if csv file exists
+		print('file found')
+		# read from csv
+		with open(filename, 'r', encoding = 'utf-8') as f:
+			for line in f:
+				if 'merchandise,price' in line:
+					continue
+				name, price = line.strip().split(',')
+				products.append([name, price])
+		print(products)
+	else:
+		print('file not found')
+	return products
 
 # user input
-while True:
-	name = input('please input merchandise: ')
-	if name == 'q':
-		break
-	price = input('please input unit price: ')
-	price = int(price)
-	products.append([name, price])
-print(products)
+def user_input(products):    
+	while True:
+		name = input('please input merchandise: ')
+		if name == 'q':
+			break
+		price = input('please input unit price: ')
+		price = int(price)
+		products.append([name, price])
+	print(products)
+	return products
 
 # print purchase record
-for p in products:
-	print('the price of', p[0], 'is', p[1])
+def print_products(products):
+	for p in products:
+		print('the price of', p[0], 'is', p[1])
 
 # write into csv
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-	f.write('merchandise,price\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+def write_file(filename, products):
+	with open('products.csv', 'w', encoding = 'utf-8') as f:
+		f.write('merchandise,price\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n')
+	
+products = read_file('products.csv')
+products = user_input(products)
+print_products(products)
+products = wrtie_file('products.csv', products)
+
