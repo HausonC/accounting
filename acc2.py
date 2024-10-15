7678c5ac939accac1ad2cb89716563154f17f73e
Hauson Chen <hauson621@hotmail.com.tw>
2024-10-15 11:50:30 +0800

    refactor

--- acc2.py
+++ acc2.py
@@ -1,48 +1,50 @@
 import os # loading operating system
+products = []
 
-# read file
+# read from csv (function中應只涵蓋單一功能)
 def read_file(filename):
-	products = []
-	if os.path.isfile(filename): # check if csv file exists
-		print('file found')
-		# read from csv
-		with open(filename, 'r', encoding = 'utf-8') as f:
-			for line in f:
-				if 'merchandise,price' in line:
-					continue
-				name, price = line.strip().split(',')
-				products.append([name, price])
-		print(products)
-	else:
-		print('file not found')
-	return products
+    with open(filename, 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if 'merchandise,price' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    return products
 
 # user input
-def user_input(products):    
-	while True:
-		name = input('please input merchandise: ')
-		if name == 'q':
-			break
-		price = input('please input unit price: ')
-		price = int(price)
-		products.append([name, price])
-	print(products)
-	return products
+def user_input(products): 
+    while True:
+        name = input('please input merchandise: ')
+        if name == 'q':
+            break
+        price = input('please input unit price: ')
+        price = int(price)
+        products.append([name, price])
+    print(products)
+    return products
 
 # print purchase record
 def print_products(products):
-	for p in products:
-		print('the price of', p[0], 'is', p[1])
+    for p in products:
+        print('the price of', p[0], 'is', p[1])
 
 # write into csv
 def write_file(filename, products):
-	with open('products.csv', 'w', encoding = 'utf-8') as f:
-		f.write('merchandise,price\n')
-		for p in products:
-			f.write(p[0] + ',' + str(p[1]) + '\n')
-	
-products = read_file('products.csv')
-products = user_input(products)
-print_products(products)
-products = wrtie_file('products.csv', products)
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        f.write('merchandise,price\n')
+        for p in products:
+            f.write(p[0] + ',' + str(p[1]) + '\n')
 
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename): # check if csv file exists
+        print('file found')
+        products = read_file(filename)
+    else:
+        print('file not found')
+    products = user_input(products)
+    print_products(products)
+    products = write_file(filename, products)
+
+main()
+# refactor (重構)
